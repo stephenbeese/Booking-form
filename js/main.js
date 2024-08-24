e804a96201407946fab146a37c77e084f6f481e4
Stephen Beese <stephenbeese@stephens-air.home>
2024-08-24 22:00:24 +0100

    Add new images and fix some styling and animations

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const properties = [
-        { id: 1, title: "Beach House", description: "Charming beachfront house in Cornwall, offering stunning ocean views, direct beach access, and cozy coastal decor. Perfect for a relaxing seaside getaway.", image: "images/card-image-1.jpg", price: 100 },
-        { id: 2, title: "Mountain Cabin", description: "Rustic cabin nestled in the serene mountains, offering breathtaking forest views, cozy fireplace, and nearby hiking trails. Ideal for a peaceful nature retreat.", image: "images/card-image-2.jpg", price: 150 },
-        { id: 3, title: "City Apartment", description: "Modern city apartment in the heart of London, featuring sleek design, high-end amenities, and easy access to shopping, dining, and cultural attractions.", image: "images/card-image-3.jpg", price: 200 },
-        { id: 4, title: "Apartment", description: "Contemporary apartment in the city center, offering stylish interiors, full kitchen, and balcony views. Perfect for urban explorers and business travelers alike.", image: "images/card-image-4.jpg", price: 159.99 },
+        { id: 1, title: "Beach House", description: "Charming beachfront house in Cornwall, offering stunning ocean views, direct beach access, and cozy coastal decor. Perfect for a relaxing seaside getaway.", image: "images/beach-house.jpg", price: 100 },
+        { id: 2, title: "Mountain Cabin", description: "Rustic cabin nestled in the serene mountains, offering breathtaking forest views, cozy fireplace, and nearby hiking trails. Ideal for a peaceful nature retreat.", image: "images/mountain-cabin.jpg", price: 150 },
+        { id: 3, title: "City Apartment", description: "Modern city apartment in the heart of London, featuring sleek design, high-end amenities, and easy access to shopping, dining, and cultural attractions.", image: "images/city-appartment.avif", price: 200 },
+        { id: 4, title: "Apartment", description: "Contemporary apartment in the city center, offering stylish interiors, full kitchen, and balcony views. Perfect for urban explorers and business travelers alike.", image: "images/appartment.jpg", price: 159.99 },
         { id: 5, title: "Flambards", description: "Quaint countryside cottage with charming garden, cozy interiors, and easy access to local attractions. Ideal for a budget-friendly escape to the countryside.", image: "images/card-image-5.webp", price: 79.99 },
         { id: 6, title: "Clambards", description: "Cozy village house with traditional decor, offering a warm ambiance, private garden, and close proximity to the local market. Great for a quiet, relaxing getaway.", image: "images/card-image-6.webp", price: 89.99 },
-        { id: 7, title: "Lake House", description: "Tranquil lakeside house with private dock, spacious living areas, and stunning water views. Perfect for fishing enthusiasts and nature lovers.", image: "images/card-image-7.jpg", price: 180 },
-        { id: 8, title: "Desert Villa", description: "Modern villa located in a desert oasis, featuring luxury amenities, private pool, and breathtaking sunset views. Great for a secluded retreat.", image: "images/card-image-8.jpg", price: 250 },
-        { id: 9, title: "Country Barn", description: "Charming renovated barn in the countryside, offering rustic decor, large garden, and a peaceful environment. Ideal for family gatherings.", image: "images/card-image-9.jpg", price: 120 },
-        { id: 10, title: "Treehouse", description: "Unique treehouse accommodation nestled among the treetops, with stunning views and a cozy interior. Perfect for a romantic getaway.", image: "images/card-image-10.jpg", price: 175 },
-        { id: 11, title: "Ski Chalet", description: "Luxury ski chalet located near popular slopes, with modern amenities, cozy fireplace, and private hot tub. Ideal for winter sports enthusiasts.", image: "images/card-image-11.jpg", price: 300 },
-        { id: 12, title: "Farmhouse", description: "Spacious farmhouse surrounded by rolling hills and farmland, featuring a large kitchen, outdoor seating area, and barn access.", image: "images/card-image-12.jpg", price: 110 },
-        { id: 13, title: "Penthouse Suite", description: "Exclusive penthouse suite in a high-rise building with panoramic city views, luxury furnishings, and a private rooftop terrace.", image: "images/card-image-13.jpg", price: 400 },
-        { id: 14, title: "Island Bungalow", description: "Secluded island bungalow surrounded by tropical plants, with private beach access and an outdoor shower. Ideal for an exotic escape.", image: "images/card-image-14.jpg", price: 220 },
-        { id: 15, title: "Historic Manor", description: "Beautifully restored historic manor with antique furnishings, expansive gardens, and elegant dining rooms. Perfect for a luxurious getaway.", image: "images/card-image-15.jpg", price: 350 }
+        { id: 7, title: "Lake House", description: "Tranquil lakeside house with private dock, spacious living areas, and stunning water views. Perfect for fishing enthusiasts and nature lovers.", image: "images/lake-house.jpg", price: 180 },
+        { id: 8, title: "Desert Villa", description: "Modern villa located in a desert oasis, featuring luxury amenities, private pool, and breathtaking sunset views. Great for a secluded retreat.", image: "images/desert-villa.jpg", price: 250 },
+        { id: 9, title: "Country Barn", description: "Charming renovated barn in the countryside, offering rustic decor, large garden, and a peaceful environment. Ideal for family gatherings.", image: "images/country-barn.jpg", price: 120 },
+        { id: 10, title: "Treehouse", description: "Unique treehouse accommodation nestled among the treetops, with stunning views and a cozy interior. Perfect for a romantic getaway.", image: "images/treehouse.jpg", price: 175 },
+        { id: 11, title: "Ski Chalet", description: "Luxury ski chalet located near popular slopes, with modern amenities, cozy fireplace, and private hot tub. Ideal for winter sports enthusiasts.", image: "images/ski-chalet.jpg", price: 300 },
+        { id: 12, title: "Farmhouse", description: "Spacious farmhouse surrounded by rolling hills and farmland, featuring a large kitchen, outdoor seating area, and barn access.", image: "images/farmhouse.jpg", price: 110 },
+        { id: 13, title: "Penthouse Suite", description: "Exclusive penthouse suite in a high-rise building with panoramic city views, luxury furnishings, and a private rooftop terrace.", image: "images/penthouse-suite.jpg", price: 400 },
+        { id: 14, title: "Island Bungalow", description: "Secluded island bungalow surrounded by tropical plants, with private beach access and an outdoor shower. Ideal for an exotic escape.", image: "images/island-bungalow.webp", price: 220 },
+        { id: 15, title: "Historic Manor", description: "Beautifully restored historic manor with antique furnishings, expansive gardens, and elegant dining rooms. Perfect for a luxurious getaway.", image: "images/historic-manor.jpg", price: 350 }
     ];
 
     const propertyContainer = document.getElementById('property-container');
@@ -68,9 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Create a property card element
     const createPropertyCard = (property) => {
         const propertyCard = document.createElement('div');
-        propertyCard.className = 'card-container col-lg-3 col-md-4 col-sm-6 col-7 mb-4 mx-3 appear';
+        propertyCard.className = 'card-container col-lg-3 col-md-4 col-sm-6 col-7 mb-4 mx-3';
         propertyCard.innerHTML = `
-            <div class="card shadow border-dark">
+            <div class="card shadow border-dark appear">
                 <img src="${property.image}" class="card-img-top border-bottom border-dark" alt="${property.title}" loading="lazy">
                 <div class="card-body pb-0">
                     <h2 class="text-black mb-0 fs-4 fw-bold mb-2">${property.title}</h2>
